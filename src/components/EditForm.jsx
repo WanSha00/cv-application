a0007c5185c5e459278a44c88c3afb5d88a90aef
WanSha00 <88984549+WanSha00@users.noreply.github.com>
2023-09-24 21:29:17 +0800

    add function to display form section based on selected form menu

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -2,6 +2,7 @@ import PersonalForm from "./edit/PersonalForm";
 import SkillsForm from "./edit/SkillsForm";
 import WorkForm from "./edit/WorkForm";
 import EducationForm from "./edit/EducationForm";
+import { useState } from "react";
 
 function EditForm({
   fullName,
@@ -27,33 +28,125 @@ function EditForm({
   eduList,
   setEduList,
 }) {
+  const [menuActive, setMenuActive] = useState({
+    personal: true,
+    skills: true,
+    work: true,
+    edu: true,
+  });
+
   return (
     <>
       <div className="edit-form-container">
-        <PersonalForm
-          fullName={fullName}
-          setFullName={setFullName}
-          currentRole={currentRole}
-          setCurrentRole={setCurrentRole}
-          summary={summary}
-          setSummary={setSummary}
-          address={address}
-          setAddress={setAddress}
-          email={email}
-          setEmail={setEmail}
-          website={website}
-          setWebsite={setWebsite}
-          phoneNo={phoneNo}
-          setPhoneNo={setPhoneNo}
-        />
-        <SkillsForm skillList={skillList} setSkillList={setSkillList} />
-        <WorkForm
-          workList={workList}
-          setWorkList={setWorkList}
-          taskList={taskList}
-          setTaskList={setTaskList}
-        />
-        <EducationForm eduList={eduList} setEduList={setEduList} />
+        <div className="menu-btn">
+          <button
+            className={
+              menuActive.personal &&
+              menuActive.skills &&
+              menuActive.work &&
+              menuActive.edu
+                ? "menu-active"
+                : ""
+            }
+            onClick={() =>
+              setMenuActive({
+                personal: true,
+                skills: true,
+                work: true,
+                edu: true,
+              })
+            }
+          >
+            All
+          </button>
+          <button
+            className={menuActive.personal ? "menu-active" : ""}
+            onClick={() =>
+              setMenuActive({
+                personal: true,
+                skills: false,
+                work: false,
+                edu: false,
+              })
+            }
+          >
+            Personal Details
+          </button>
+          <button
+            className={menuActive.skills ? "menu-active" : ""}
+            onClick={() =>
+              setMenuActive({
+                personal: false,
+                skills: true,
+                work: false,
+                edu: false,
+              })
+            }
+          >
+            Skills
+          </button>
+          <button
+            className={menuActive.work ? "menu-active" : ""}
+            onClick={() =>
+              setMenuActive({
+                personal: false,
+                skills: false,
+                work: true,
+                edu: false,
+              })
+            }
+          >
+            Work Experience
+          </button>
+          <button
+            className={menuActive.edu ? "menu-active" : ""}
+            onClick={() =>
+              setMenuActive({
+                personal: false,
+                skills: false,
+                work: false,
+                edu: true,
+              })
+            }
+          >
+            Education
+          </button>
+        </div>
+        {menuActive.personal && (
+          <PersonalForm
+            fullName={fullName}
+            setFullName={setFullName}
+            currentRole={currentRole}
+            setCurrentRole={setCurrentRole}
+            summary={summary}
+            setSummary={setSummary}
+            address={address}
+            setAddress={setAddress}
+            email={email}
+            setEmail={setEmail}
+            website={website}
+            setWebsite={setWebsite}
+            phoneNo={phoneNo}
+            setPhoneNo={setPhoneNo}
+          />
+        )}
+
+        {menuActive.skills && (
+          <SkillsForm skillList={skillList} setSkillList={setSkillList} />
+        )}
+
+        {menuActive.work && (
+          <WorkForm
+            workList={workList}
+            setWorkList={setWorkList}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        )}
+
+        {menuActive.edu && (
+          <EducationForm eduList={eduList} setEduList={setEduList} />
+        )}
       </div>
     </>
   );
